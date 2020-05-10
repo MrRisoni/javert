00ba45e28cb04c50766ed00ac7fbcdcbd70e5eba
MrRisoni <vanguelisk@gmail.com>
2020-05-10 19:22:43 +0300

    java class for df -h

--- agent/src/main/java/com/example/restservice/GreetingController.java
+++ agent/src/main/java/com/example/restservice/GreetingController.java
@@ -8,11 +8,57 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 @RestController
 public class GreetingController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
+
+
+	@GetMapping("/partitions")
+	public ArrayList<Partition> partiions(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+		ArrayList<Partition> partitions = new ArrayList<>();
+
+
+		try {
+			String dfOut;
+			Process p;
+					 p = Runtime.getRuntime().exec("df -h");
+					 BufferedReader br = new BufferedReader(
+							 new InputStreamReader(p.getInputStream()));
+					 while ((dfOut = br.readLine()) != null) {
+						 System.out.println("---------------");
+							 System.out.println("line: " + dfOut);
+							 String[] split = dfOut.split(" ");
+
+							 System.out.println(split);
+							 System.out.println(split[3]);
+							 System.out.println(split.length);
+
+							 Partition prt = new Partition(split[0],split[1],split[2],split[3],split[4]);
+							 partitions.add(prt);
+						 }
+					 p.waitFor();
+					 System.out.println ("exit: " + p.exitValue());
+					 p.destroy();
+
+					 return partitions;
+
+			 } catch (Exception e) {
+				 System.out.println("Error");
+				 e.printStackTrace();
+				 return partitions;
+
+			 }
+
+
+	}
+
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
---
+++ agent/src/main/java/com/example/restservice/PartitionList.java
@@ -0,0 +1,17 @@
+package com.example.restservice;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PartitionList {
+
+        ArrayList<Partition> partitions = new ArrayList<>();
+
+        public PartitionList()
+        {
+        }
+
+        
+}
