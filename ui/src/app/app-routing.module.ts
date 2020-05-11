be7a3cac33e55816cd8cc184c8b38ac663c096d4
MrRisoni <vanguelisk@gmail.com>
2020-05-11 14:01:36 +0300

    set routes

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {HostComponent} from './host/host.component';
+import {AppComponent} from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+      {path: '', component: AppComponent},
+    {path: 'host', component: HostComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
